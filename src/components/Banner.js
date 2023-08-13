b4268fbb36692be6d001b3d7485edfd370241470
Pell35 <pjelliott11@gmail.com>
2023-08-13 00:17:50 -0500

    Update banner, changed syntax

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,7 @@
-const Banner = () => {
+function Banner = () => {
     return(
     <div>
         <h>Orbit Report</h>
     <p>Click on the buttons to see the satellites in that orbit type</p>
     </div>);    
-} export default 
+} export default function
