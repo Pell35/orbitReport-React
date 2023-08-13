65344342856f72649f45c4840c3cb0c225871361
Pell35 <pjelliott11@gmail.com>
2023-08-13 01:54:35 -0500

    Task #4

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,10 +1,20 @@
+import satData from "./satData";
+import filterByType from "src/App.js";
+import setSat from "src/App.js";
+import displaySats from "src/App.js";
 const Buttons = () => {
-  return (
-    <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
-      </div>
-  );
+  {displaySats.map((sat, id) => {
+    return (
+      <button onClick={() => filterByType(sat)} key={id}>
+        {sat} Orbit
+      </button>
+    );
+  })
+}
+return (
+  <button onClick={() => setSat(satData)} key={id}>
+    {sat} Not Sure
+  </button>
 };
 
 export default Buttons;
