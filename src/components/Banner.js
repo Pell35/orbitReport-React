45e195e635ef6c48c6b05342f7816c8b3c0d4b8d
Pell35 <pjelliott11@gmail.com>
2023-08-13 00:14:58 -0500

    Added Banner Component

---
+++ src/components/Banner.js
@@ -0,0 +1,8 @@
+function Banner(){
+    return(
+    <div>
+        <h>Orbit Report</h>
+    <p>Click on the buttons to see the satellites in that orbit type</p>
+    </div>);    
+}
+export default 
