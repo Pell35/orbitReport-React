f97b5011bceb7c7aef48e8ba84b97e29e9ee356c
Pell35 <pjelliott11@gmail.com>
2023-08-13 00:21:46 -0500

    Trying Banner again

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,8 @@
-function Banner = () => {
+export default 
+function Banner() {
     return(
     <div>
         <h>Orbit Report</h>
     <p>Click on the buttons to see the satellites in that orbit type</p>
     </div>);    
-} export default function
+} 
