82f8a90ed60457f154e6aea00cf528478f284415
Pell35 <pjelliott11@gmail.com>
2023-08-14 02:27:33 -0500

    Task 5 without conditional

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,21 +1,25 @@
 const Table = ({ sat }) => {
-  return (
-      <table>
-       <thead>
-        <tr>
-          <th>Name</th>
-          <th>Type of Satellite</th>
-          <th>Launch Date</th>
-          <th>Status</th>  
-        </tr>
-        </thead>
-        <tbody>
- 
-        <tr> 
-          <td></td>
-        </tr>
-        </tbody>
-      </table>
+  return (    <table>
+    <thead>
+     <tr>
+       <th>Name</th>
+       <th>Type of Satellite</th>
+       <th>Launch Date</th>
+       <th>Status</th>  
+     </tr>
+     </thead>
+     <tbody> 
+    {sat.map((id, data) => {
+       return(
+    <tr  key={id}>
+     <td>{data.name}</td>
+     <td>{data.type}</td>
+     <td>{data.launchDate}</td>
+     <td>{data.status}</td>
+      </tr>
+       );})}
+     </tbody>
+   </table>
   );
 };
 
